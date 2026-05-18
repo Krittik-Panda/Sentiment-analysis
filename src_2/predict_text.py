c9ab6fad4410a6039e0ba65cb5e74e3194a81fa2
Rik <krittikpanda1@gmail.com>
2026-05-19 00:30:16 +0530

    logic is added for neutral class

--- src_2/predict_text.py
+++ src_2/predict_text.py
@@ -11,17 +11,35 @@ stopwords = load_stopwords("stop-words-list.txt")
 def predict():
         input_text = str(input("Enter a text : "))
         clean_input_text = clean_text(input_text, stopwords)
-        # check if the text is empty after clean or not
-        if clean_input_text.strip() == "":
-            return "Text contains no known vocabulary."
+  
 
         X = vectorizer.transform([clean_input_text]) # must pass a list
+        #if X is not in vocabulary then all will be zero
+        if X.nnz == 0:
+            print("Text contains no known vocabulary.")
+            return
+
+
         prediction = model.predict(X)
         probability = model.predict_proba(X)
 
-        print(f"Text is {prediction[0]}")
-        print(f"probability is {probability}")
+        neg_prob = probability[0][0]
+        pos_prob = probability[0][1]
 
+        if 0.45 <= neg_prob <= 0.55 or 0.45 <= pos_prob <= 0.55:
+            print("The text is neutral")
+            print(f"Probability is {probability}") 
+        elif 0.50 <= neg_prob < 0.60:
+            print("The text is neutral to negative")
+            print(f"Probability is {probability}")
+
+        elif 0.50 <= pos_prob <= 0.60:
+            print("The text is neutral to positive")
+            print(f"Probability is {probability}")
+
+        else:
+            print(f"Text is {prediction[0]}")
+            print(f"Probability is {probability}")
 
 
 if __name__=="__main__":
