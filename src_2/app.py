e28eacee19e891867b0a02a01f1ba66e328f1699
Rik <krittikpanda1@gmail.com>
2026-05-19 22:33:06 +0530

    fix NameError

--- src_2/app.py
+++ src_2/app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template , url_for , redirect
+from flask import Flask , render_template , url_for , redirect , request
 import pickle
 import os
 from preprocess import clean_text, load_stopwords
@@ -18,17 +18,18 @@ def welcome():
     return render_template("index.html")
 
 @app.route("/result/<string:label>")  
-def result():
-    return render_template("result.html")  
+def result(label):
+    return render_template("result.html" , label = label)  
 
 @app.route("/predict/", methods = ["POST"])
 def predict():
-        text = str(request.index(["tweet"]))
+        input_text = str(request.form.get("tweet"))
+
         clean_input_text = clean_text(input_text, stopwords) # clean 
         X = vectorizer.transform([clean_input_text])
         if X.nnz == 0:
-            print("Text contains no known vocabulary.")
-            return
+            labell = "Text contains no known vocabulary."
+            return redirect(url_for('result' , label = labell))
 
 
         prediction = model.predict(X)
@@ -38,22 +39,22 @@ def predict():
         pos_prob = probability[0][1]
 
         if 0.45 <= neg_prob <= 0.55 or 0.45 <= pos_prob <= 0.55:
-            label = "The text is neutral"
-            return redirect(url_for('result' , label = label))
+            labell = "The text is neutral"
+            return redirect(url_for('result' , label = labell))
             #print(f"Probability is {probability}") 
         elif 0.50 <= neg_prob < 0.60:
-            label = "The text is neutral to negative"
-            return redirect(url_for('result' , label = label))
+            labell = "The text is neutral to negative"
+            return redirect(url_for('result' , label = labell))
             #print(f"Probability is {probability}")
 
         elif 0.50 <= pos_prob <= 0.60:
-            label = "The text is neutral to positive"
-            return redirect(url_for('result' , label = label))
+            labell = "The text is neutral to positive"
+            return redirect(url_for('result' , label = labell))
             #print(f"Probability is {probability}")
 
         else:
-            label = f"Text is {prediction[0]}"
-            return redirect(url_for('result' , label = label))
+            labell = f"Text is {prediction[0]}"
+            return redirect(url_for('result' , label = labell))
             #print(f"Probability is {probability}")
 
 
